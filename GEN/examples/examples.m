fbdf164a2f3551fdf2063dde35ef12dd7aa7b510
abehbood <abehbood@xilinx.com>
2023-02-04 17:10:57 -0800

    Adding examples.html file

---
+++ GEN/examples/examples.m
@@ -0,0 +1,25 @@
+%%
+% <html><h1>Vitis Model Composer Examples and Tutorials</h1></html>
+%
+%%
+%
+% <html><h1>Open Examples and Tutorials</h1></html>
+%
+% Click the button below to open Examples and Tutorials for Vitis Model
+% Composer. If there are newer versions of the Examples and Tutorials on
+% Xilinx GitHub, and you have access to the internet, you will be prompted
+% to download them to a directory of your choice. You can also directly
+% visit the <https://github.com/Xilinx/Vitis_Model_Composer GitHub
+% repository> and pick the branch in GitHub that corresponds to your
+% version of Vitis Model Composer.
+%
+% Note that to open and explore AI Engine examples, you must have Vitis
+% installed and use a Linux based OS.
+%
+% <html> <style> #rcorners3 {
+%    border-radius: 100px; background: #d0d028; padding: 15px; width:
+%    200px; height: 15px; font-size:130%
+% } </style> <center><p id="rcorners3"> <a
+% href="matlab:XmcClassExamples();">Examples and
+% Tutorials </a> </p> </center> </html>
+%
